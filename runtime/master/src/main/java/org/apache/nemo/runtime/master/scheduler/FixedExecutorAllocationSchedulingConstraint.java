ec27c55dfbb251cf95bc7ce20fe9a503411d8b2c
Lemarais <goehd4551@gmail.com>
2021-06-23 17:14:13 +0900

    custom policy for experiment

---
+++ runtime/master/src/main/java/org/apache/nemo/runtime/master/scheduler/FixedExecutorAllocationSchedulingConstraint.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.nemo.runtime.master.scheduler;
+
+import org.apache.nemo.common.ir.executionproperty.AssociatedProperty;
+import org.apache.nemo.common.ir.vertex.executionproperty.ExecutorSelectionProperty;
+import org.apache.nemo.runtime.common.plan.Task;
+import org.apache.nemo.runtime.master.resource.ExecutorRepresenter;
+
+import javax.inject.Inject;
+
+/**
+ * This policy finds executor that has free slot for a Task.
+ */
+@AssociatedProperty(ExecutorSelectionProperty.class)
+public final class FixedExecutorAllocationSchedulingConstraint implements SchedulingConstraint {
+
+  @Inject
+  private FixedExecutorAllocationSchedulingConstraint() {
+  }
+
+  @Override
+  public boolean testSchedulability(final ExecutorRepresenter executor, final Task task) {
+    final int ExecutorIndex = task.getPropertyValue(ExecutorSelectionProperty.class)
+      .orElse(-1);
+
+    return ExecutorIndex < 0 || ExecutorIndex == Integer.parseInt(executor.getExecutorId().substring(8));
+  }
+}
